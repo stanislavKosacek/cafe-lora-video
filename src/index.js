02f095f3fde648bcb8da3dffdc4ddac8cb2451c1
Stanislav Kosáček <stanislav.kosacek@icloud.com>
2022-05-01 17:57:33 +0200

    úkol 1.3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtnElm = document.querySelector('#nav-btn');
+navBtnElm.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const navLinkElms = document.querySelectorAll('nav a');
+navLinkElms.forEach((navLinkElm) => {
+  navLinkElm.addEventListener('click', () => {
+    document.querySelector('nav').classList.add('nav-closed');
+  });
+});
