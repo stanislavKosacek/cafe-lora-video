6d2ab5e3d6ff771a5891236c9cd8cffa564481b8
Stanislav Kosáček <stanislav.kosacek@icloud.com>
2022-05-01 18:38:18 +0200

    úkol 2.5

--- src/index.js
+++ src/index.js
@@ -14,45 +14,12 @@ navLinkElms.forEach((navLinkElm) => {
 });
 
 const drinksListElm = document.querySelector('.drinks-list');
-const drinks = [
-  {
-    id: 'cappuccino',
-    name: 'Cappuccino',
-    ordered: false,
-    layers: [
-      {
-        color: '#feeeca',
-        label: 'mléčná pěna',
-      },
-      {
-        color: '#fed7b0',
-        label: 'teplé mléko',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/cappuccino.png',
-  },
-  {
-    id: 'romano',
-    name: 'Romano',
-    ordered: false,
-    layers: [
-      {
-        color: '#fbdf5b',
-        label: 'citrón',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-  },
-];
 
-drinks.forEach((drink) => {
-  drinksListElm.appendChild(Drink(drink));
-});
+fetch('https://apps.kodim.cz/daweb/cafelora/api/drinks')
+  .then((response) => response.json())
+  .then((data) => {
+    const drinks = data.results;
+    drinks.forEach((drink) => {
+      drinksListElm.appendChild(Drink(drink));
+    });
+  });
