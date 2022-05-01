ad572ef959956b17e9c5d9f84ea585b99ffd347f
Stanislav Kosáček <stanislav.kosacek@icloud.com>
2022-05-01 18:34:44 +0200

    úkol 2.4

--- src/index.js
+++ src/index.js
@@ -14,20 +14,45 @@ navLinkElms.forEach((navLinkElm) => {
 });
 
 const drinksListElm = document.querySelector('.drinks-list');
-const drink = {
-  id: 'romano',
-  name: 'Romano',
-  ordered: false,
-  image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-  layers: [
-    {
-      color: '#fbdf5b',
-      label: 'citrón',
-    },
-    {
-      color: '#613916',
-      label: 'espresso',
-    },
-  ],
-};
-drinksListElm.appendChild(Drink(drink));
+const drinks = [
+  {
+    id: 'cappuccino',
+    name: 'Cappuccino',
+    ordered: false,
+    layers: [
+      {
+        color: '#feeeca',
+        label: 'mléčná pěna',
+      },
+      {
+        color: '#fed7b0',
+        label: 'teplé mléko',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/cappuccino.png',
+  },
+  {
+    id: 'romano',
+    name: 'Romano',
+    ordered: false,
+    layers: [
+      {
+        color: '#fbdf5b',
+        label: 'citrón',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
+  },
+];
+
+drinks.forEach((drink) => {
+  drinksListElm.appendChild(Drink(drink));
+});
